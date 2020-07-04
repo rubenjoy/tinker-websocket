2ec25a9241679590a3a5502d53fabf18503567e0
Ruben Juliarto <ruben@accelbyte.net>
2020-07-04 08:05:54 +0700

    fix warning messages

--- echo/server.go
+++ echo/server.go
@@ -26,71 +26,56 @@ func echoHello(w http.ResponseWriter, r *http.Request) {
 		attachLogHandler(conn)
 	}
 
-	conn.WriteMessage(websocket.TextMessage, []byte("hello"))
-	conn.WriteMessage(websocket.CloseMessage,
+	_ = conn.WriteMessage(websocket.TextMessage, []byte("hello"))
+	_ = conn.WriteMessage(websocket.CloseMessage,
 		websocket.FormatCloseMessage(websocket.CloseNormalClosure, "bye"))
-}
-
-func loopRead(conn *websocket.Conn, tag string) {
-	for {
-		mt, msg, err := conn.ReadMessage()
-		if err != nil {
-			log.Printf("tag: '%s', read message: '%s'", tag, err)
-			return
-		}
-		if websocket.TextMessage == mt {
-			log.Printf("tag: '%s', message: %s", tag, msg)
-		}
-	}
 }
 
 func attachLogHandler(conn *websocket.Conn) {
 	conn.SetPongHandler(logPongMessage)
 	conn.SetPingHandler(logPingMessage)
-	conn.SetCloseHandler(dumpCloseMessage)
+	conn.SetCloseHandler(logCloseMessage)
 }
 
-func dumpCloseMessage(code int, text string) error {
+func logCloseMessage(code int, text string) error {
 	log.Printf("close, code: %d, text: %s\n", code, text)
 	return nil
 }
 
 type closeHandler func(int, string) error
-type pongHandler func(string) error
 type pingHandler func(string) error
 
 func sendBackClose(conn *websocket.Conn) closeHandler {
 	return func(i int, s string) error {
-		conn.WriteControl(websocket.CloseMessage, websocket.FormatCloseMessage(i, s), time.Now().Add(2*time.Second))
-		return dumpCloseMessage(i, s)
+		err := conn.WriteControl(websocket.CloseMessage,
+			websocket.FormatCloseMessage(i, s), time.Now().Add(2*time.Second))
+		if err != nil {
+			log.Println("write control: ", err)
+		}
+		return logCloseMessage(i, s)
 	}
 }
 
 func logPongMessage(data string) error {
-	log.Println("pong message: ", data)
-	return nil
-}
-
-func sendBackPong(conn *websocket.Conn) pongHandler {
-	return func(text string) error {
-		conn.WriteMessage(websocket.PongMessage, []byte(text))
-		return logPongMessage(text)
-	}
+	return dumpMessage("pong message: ", data)
 }
 
 func logPingMessage(data string) error {
-	log.Println("ping message: ", data)
-	return nil
+	return dumpMessage("ping message: ", data)
 }
 
-func dumpMessage(msg string) error {
+func dumpMessage(msg ...string) error {
 	log.Println(msg)
 	return nil
 }
 
 func responseWithPong(conn *websocket.Conn) pingHandler {
 	return func(text string) error {
-		return logPingMessage(text)
+		err := conn.WriteControl(websocket.PongMessage, []byte(text), time.Now().Add(2*time.Second))
+		if err != nil {
+			log.Println("write control: ", err)
+		}
+		return dumpMessage("ping message: ", text)
 	}
 }
 
@@ -115,8 +100,8 @@ func ping(w http.ResponseWriter, r *http.Request) {
 		attachLogHandler(conn)
 	}
 
-	conn.WriteMessage(websocket.PingMessage, []byte("pmci"))
-	conn.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, "bye"))
+	_ = conn.WriteMessage(websocket.PingMessage, []byte("pmci"))
+	_ = conn.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, "bye"))
 }
 
 // echoMessage upgrades the HTTP request into a websocket
@@ -132,6 +117,9 @@ func echoMessage(w http.ResponseWriter, r *http.Request) {
 	defer conn.Close()
 	if dropControlMessage(r) {
 		attachLogHandler(conn)
+	} else {
+		conn.SetPingHandler(responseWithPong(conn))
+		conn.SetCloseHandler(sendBackClose(conn))
 	}
 
 	for {
@@ -165,7 +153,7 @@ func flood(w http.ResponseWriter, r *http.Request) {
 
 	ticker := time.NewTicker(time.Second)
 	defer ticker.Stop()
-
+	// TODO flooding the client with time message in every second for X seconds
 }
 
 func main() {
